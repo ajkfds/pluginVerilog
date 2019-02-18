b8a6ac96eb3f3007880e37e99e03feec2670e45e
ajkfds <ajkfds2018@gmail.com>
2019-02-19 06:22:52 +0900

    fix global.image name

--- Verilog/ParsedDocument.cs
+++ Verilog/ParsedDocument.cs
@@ -61,16 +61,16 @@ namespace pluginVerilog.Verilog
                 switch (message.Type)
                 {
                     case Message.MessageType.Error:
-                        ret.Add(new codeEditor.CodeEditor.PopupItem(message.Text, System.Drawing.Color.Red, Global.IconImages.ExclamationBox, ajkControls.IconImage.ColorStyle.Red));
+                        ret.Add(new codeEditor.CodeEditor.PopupItem(message.Text, System.Drawing.Color.Red, Global.Icons.ExclamationBox, ajkControls.IconImage.ColorStyle.Red));
                         break;
                     case Message.MessageType.Warning:
-                        ret.Add(new codeEditor.CodeEditor.PopupItem(message.Text, System.Drawing.Color.Orange, Global.IconImages.ExclamationBox, ajkControls.IconImage.ColorStyle.Orange));
+                        ret.Add(new codeEditor.CodeEditor.PopupItem(message.Text, System.Drawing.Color.Orange, Global.Icons.ExclamationBox, ajkControls.IconImage.ColorStyle.Orange));
                         break;
                     case Message.MessageType.Hint:
-                        ret.Add(new codeEditor.CodeEditor.PopupItem(message.Text, System.Drawing.Color.Blue, Global.IconImages.ExclamationBox, ajkControls.IconImage.ColorStyle.Blue));
+                        ret.Add(new codeEditor.CodeEditor.PopupItem(message.Text, System.Drawing.Color.Blue, Global.Icons.ExclamationBox, ajkControls.IconImage.ColorStyle.Blue));
                         break;
                     case Message.MessageType.Notice:
-                        ret.Add(new codeEditor.CodeEditor.PopupItem(message.Text, System.Drawing.Color.Green, Global.IconImages.ExclamationBox, ajkControls.IconImage.ColorStyle.Green));
+                        ret.Add(new codeEditor.CodeEditor.PopupItem(message.Text, System.Drawing.Color.Green, Global.Icons.ExclamationBox, ajkControls.IconImage.ColorStyle.Green));
                         break;
                 }
             }
@@ -242,6 +242,16 @@ namespace pluginVerilog.Verilog
             foreach (Variables.Parameter parameter in nameSpace.Module.LocalParameters.Values)
             {
                 items.Add(newItem(parameter.Name, CodeDrawStyle.ColorType.Paramater));
+            }
+
+            foreach (Function function in nameSpace.Module.Functions.Values)
+            {
+                items.Add(newItem(function.Name, CodeDrawStyle.ColorType.Identifier));
+            }
+
+            foreach (Task task in nameSpace.Module.Tasks.Values)
+            {
+                items.Add(newItem(task.Name, CodeDrawStyle.ColorType.Identifier));
             }
         }
 
