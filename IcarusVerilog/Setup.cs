560af95ccbd27a634013fe8a33f26471fef02a54
ajkfds <ajkfds2018@gmail.com>
2019-12-10 06:29:03 +0900

    change icarusverilog icon

--- IcarusVerilog/Setup.cs
+++ IcarusVerilog/Setup.cs
@@ -9,6 +9,7 @@ namespace pluginVerilog.IcarusVerilog
     public static class Setup
     {
         public static string BinPath = @"C:\iverilog\bin\";
+        public static string GtkWaveBinPath = @"C:\iverilog\gtkwave\bin";
         public static string SimulationPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal) + @"\Simulation\icarusVerilog";
     }
 }
