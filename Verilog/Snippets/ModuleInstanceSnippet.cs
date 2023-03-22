634c43630dac2a0e55bf929de7fe327497cc44be
ajkfds <ajkfds2018@gmail.com>
2023-03-23 05:30:06 +0900

    [fix] module instance snippets

--- Verilog/Snippets/ModuleInstanceSnippet.cs
+++ Verilog/Snippets/ModuleInstanceSnippet.cs
@@ -18,54 +18,105 @@ namespace pluginVerilog.Verilog.Snippets
         private CodeDocument document;
 
         // initial value for {n}
-        private List<string> initials = new List<string> { "module_name", "instance_name", "();"  };
+        private List<string> initials = new List<string> {  };
 
         public override void Apply(CodeDocument codeDocument)
         {
-            document = codeDocument;
+            base.Apply(codeDocument);
+            List<ToolItem> items = new List<ToolItem>();
 
-            string indent = "";
-            if (document.GetCharAt(document.GetLineStartIndex(document.GetLineAt(document.CaretIndex))) == '\t')
+            codeEditor.Data.Project project = codeDocument.TextFile.Project;
+            ProjectProperty projectProperty = project.ProjectProperties[Plugin.StaticID] as ProjectProperty;
+
+            List<string> moduleNmaes = projectProperty.GetModuleNameList();
+            foreach(string moduleName in moduleNmaes)
             {
-                indent = "\t";
+                items.Add(new ModuleInastnaceSelectionItem(moduleName,this));
             }
 
-            string replaceText =
-                indent + "{0} {1}\r\n" +
-                indent + "{2};\r\n";
+            codeEditor.Controller.CodeEditor.ForceOpenCustomSelection(ApplyTool, items);
+        }
+
+        public class ModuleInastnaceSelectionItem : codeEditor.CodeEditor.ToolItem
+        {
+            public ModuleInastnaceSelectionItem(string text, ModuleInstanceSnippet moduleInstanceSnippet) : base(text)
+            {
+                snippet = moduleInstanceSnippet;
+            }
+
+            private ModuleInstanceSnippet snippet;
+            public override void Apply(CodeDocument codeDocument)
+            {
+                snippet.ApplyModuleInstance(codeDocument, Text);
+            }
+        }
+
+        public void ApplyModuleInstance(CodeDocument codeDocument, string Text)
+        {
+            codeEditor.Data.Project project = codeDocument.TextFile.Project;
+
+            ProjectProperty projectProperty = project.ProjectProperties[Plugin.StaticID] as ProjectProperty;
+
+            Data.VerilogFile targetFile = projectProperty.GetFileOfModule(Text) as Data.VerilogFile;
+            if (targetFile == null) return;
+
+            string instanceName = Text + "_";
+            {
+                Data.IVerilogRelatedFile vfile = codeEditor.Controller.CodeEditor.GetTextFile() as Data.IVerilogRelatedFile;
+                if (vfile == null) return;
+
+                ParsedDocument parenetParsedDocument = vfile.VerilogParsedDocument;
+                Module module = parenetParsedDocument.GetModule(vfile.CodeDocument.CaretIndex);
+
+                int instanceCount = 0;
+                while (module.ModuleInstantiations.ContainsKey(Text + "_" + instanceCount.ToString()))
+                {
+                    instanceCount++;
+                }
+                instanceName = Text + "_" + instanceCount.ToString();
+            }
+
+            Verilog.ParsedDocument targetParsedDocument = targetFile.ParsedDocument as Verilog.ParsedDocument;
+            if (targetParsedDocument == null) return;
+
+            Verilog.Module targetModule = targetParsedDocument.Modules[Text];
+            if (targetModule == null) return;
+
+            string replaceText = getReplaceText(targetModule, instanceName);
 
 
             int index = codeDocument.CaretIndex;
 
-            for (int i = 0; i < initials.Count; i++)
+            if (initials.Count == 0)
             {
-                string target = "{" + i.ToString() + "}";
-                if (!replaceText.Contains(target)) break;
-                startIndexs.Add(index + replaceText.IndexOf(target));
-                lastIndexs.Add(index + replaceText.IndexOf(target) + initials[i].Length - 1);
-                replaceText = replaceText.Replace(target, initials[i]);
+                codeDocument.Replace(index, 0, 0, replaceText);
+                codeEditor.Controller.CodeEditor.AbortInteractiveSnippet();
+            }
+            else
+            {
+                for (int i = 0; i < initials.Count; i++)
+                {
+                    string target = "{" + i.ToString() + "}";
+                    if (!replaceText.Contains(target)) break;
+                    startIndexs.Add(index + replaceText.IndexOf(target));
+                    lastIndexs.Add(index + replaceText.IndexOf(target) + initials[i].Length - 1);
+                    replaceText = replaceText.Replace(target, initials[i]);
+                }
+
+                codeDocument.Replace(index, 0, 0, replaceText);
+                codeDocument.CaretIndex = startIndexs[0];
+                codeDocument.SelectionStart = startIndexs[0];
+                codeDocument.SelectionLast = lastIndexs[0] + 1;
+
+                codeEditor.Controller.CodeEditor.ClearHighlight();
+                for (int i = 0; i < startIndexs.Count; i++)
+                {
+                    codeEditor.Controller.CodeEditor.AppendHighlight(startIndexs[i], lastIndexs[i]);
+                }
             }
 
-            codeDocument.Replace(index, 0, 0, replaceText);
-            codeDocument.CaretIndex = startIndexs[0];
-            codeDocument.SelectionStart = startIndexs[0];
-            codeDocument.SelectionLast = lastIndexs[0] + 1;
+            codeEditor.Controller.CodeEditor.RequestReparse();
 
-            // set highlights for {n} texts
-            codeEditor.Controller.CodeEditor.ClearHighlight();
-            for (int i = 0; i < startIndexs.Count; i++)
-            {
-                codeEditor.Controller.CodeEditor.AppendHighlight(startIndexs[i], lastIndexs[i]);
-            }
-
-
-            base.Apply(codeDocument);
-
-            codeEditor.Controller.CodeEditor.ForceOpenAutoComplete(new List<AutocompleteItem>
-                        {
-                            new AutocompleteItem("MODULE1",(byte)CodeDrawStyle.ColorType.Keyword ,Color.Red),
-                            new AutocompleteItem("MODULE2",(byte)CodeDrawStyle.ColorType.Keyword ,Color.Red),
-                        });
         }
 
 
@@ -86,9 +137,7 @@ namespace pluginVerilog.Verilog.Snippets
             {
                 if (e.KeyCode == Keys.Return || e.KeyCode == Keys.Escape)
                 {
-                    bool moved;
-                    moveToNextHighlight(out moved);
-                    if (!moved) codeEditor.Controller.CodeEditor.AbortInteractiveSnippet();
+                    codeEditor.Controller.CodeEditor.AbortInteractiveSnippet();
                     e.Handled = true;
                 }
             }
@@ -101,20 +150,9 @@ namespace pluginVerilog.Verilog.Snippets
         {
             if (e.Handled) // closed
             {
-                int i = codeEditor.Controller.CodeEditor.GetHighlightIndex(document.CaretIndex);
+               int i = codeEditor.Controller.CodeEditor.GetHighlightIndex(document.CaretIndex);
                 switch (i)
                 {
-                    case 0: // module name
-                        codeEditor.Controller.CodeEditor.SelectHighlight(1);
-                        break;
-                    case 1: // instance name
-                        int start, last;
-                        codeEditor.Controller.CodeEditor.GetHighlightPosition(0, out start, out last);
-                        string text = document.CreateString(start, last - start + 1);
-                        codeEditor.Controller.CodeEditor.GetHighlightPosition(1, out start, out last);
-                        document.Replace(start, last - start + 1, 0, text);
-                        codeEditor.Controller.CodeEditor.AbortInteractiveSnippet();
-                        break;
                     default:
                         codeEditor.Controller.CodeEditor.AbortInteractiveSnippet();
                         break;
@@ -133,209 +171,53 @@ namespace pluginVerilog.Verilog.Snippets
             codeEditor.Controller.CodeEditor.SelectHighlight(i);
             moved = true;
         }
-        
-        //private CodeDocument document;
-        //codeEditor.Data.Project project;
 
-        //public override void Apply(CodeDocument codeDocument)
-        //{
-        //    document = codeDocument;
-        //    project = codeDocument.TextFile.Project;
+        private void ApplyTool(object sender, EventArgs e)
+        {
 
-        //    ProjectProperty projectProperty = project.GetProjectProperty(Plugin.StaticID) as ProjectProperty;
-
-        //    Data.VerilogFile targetFile = projectProperty.GetFileOfModule(Text) as Data.VerilogFile;
-        //    if (targetFile == null) return;
-
-        //    string instanceName = Text + "_";
-        //    {
-        //        Data.IVerilogRelatedFile vfile = codeEditor.Controller.CodeEditor.GetTextFile() as Data.IVerilogRelatedFile;
-        //        if (vfile == null) return;
-
-        //        ParsedDocument parenetParsedDocument = vfile.VerilogParsedDocument;
-        //        Module module = parenetParsedDocument.GetModule(vfile.CodeDocument.CaretIndex);
-
-        //        int instanceCount = 0;
-        //        while (module.ModuleInstantiations.ContainsKey(Text + "_" + instanceCount.ToString()))
-        //        {
-        //            instanceCount++;
-        //        }
-        //        instanceName = Text + "_" + instanceCount.ToString();
-        //    }
-
-        //    Verilog.ParsedDocument targetParsedDocument = targetFile.ParsedDocument as Verilog.ParsedDocument;
-        //    if (targetParsedDocument == null) return;
-
-        //    Verilog.Module targetModule = targetParsedDocument.Modules[Text];
-        //    if (targetModule == null) return;
-
-        //    string replaceText = getReplaceText(targetModule,instanceName);
+        }
 
 
-        //    int index = codeDocument.CaretIndex;
+        private string getReplaceText(Module module, string instanceName)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(module.Name);
+            sb.Append(" ");
+            if (module.PortParameterNameList.Count > 0)
+            {
+                sb.Append("#(\r\n");
+                bool first = true;
+                foreach (string portName in module.PortParameterNameList)
+                {
+                    if (!first) sb.Append(",\r\n");
+                    sb.Append("\t");
+                    sb.Append(".");
+                    sb.Append(portName);
+                    sb.Append("\t( ");
+                    sb.Append(" )");
+                    first = false;
+                }
+                sb.Append("\r\n) ");
+            }
 
-        //    if(initials.Count == 0)
-        //    {
-        //        codeDocument.Replace(index, 0, 0, replaceText);
-        //        codeEditor.Controller.CodeEditor.AbortInteractiveSnippet();
-        //    }
-        //    else
-        //    {
-        //        for (int i = 0; i < initials.Count; i++)
-        //        {
-        //            string target = "{" + i.ToString() + "}";
-        //            if (!replaceText.Contains(target)) break;
-        //            startIndexs.Add(index + replaceText.IndexOf(target));
-        //            lastIndexs.Add(index + replaceText.IndexOf(target) + initials[i].Length - 1);
-        //            replaceText = replaceText.Replace(target, initials[i]);
-        //        }
+            sb.Append("{0}");
+            initials.Add(instanceName);
 
-        //        codeDocument.Replace(index, 0, 0, replaceText);
-        //        codeDocument.CaretIndex = startIndexs[0];
-        //        codeDocument.SelectionStart = startIndexs[0];
-        //        codeDocument.SelectionLast = lastIndexs[0] + 1;
+            sb.Append(" (\r\n");
+            int j = 0;
+            foreach (Verilog.Variables.Port port in module.Ports.Values)
+            {
+                sb.Append("\t.");
+                sb.Append(port.Name);
+                sb.Append("\t(  )");
+                if (j != module.Ports.Count - 1) sb.Append(",");
+                sb.Append("\r\n");
+                j++;
+            }
+            sb.Append(");");
 
-        //        codeEditor.Controller.CodeEditor.ClearHighlight();
-        //        for (int i = 0; i < startIndexs.Count; i++)
-        //        {
-        //            codeEditor.Controller.CodeEditor.AppendHighlight(startIndexs[i], lastIndexs[i]);
-        //        }
-        //    }
+            return sb.ToString();
+        }
 
-        //    codeEditor.Controller.CodeEditor.RequestReparse();
-        //    base.Apply(codeDocument);
-
-        //}
-
-        //private string getReplaceText(Module module,string instanceName)
-        //{
-        //    int i = 0;
-        //    StringBuilder sb = new StringBuilder();
-        //    sb.Append(" ");
-        //    if (module.PortParameterNameList.Count > 0)
-        //    {
-        //        sb.Append("#(\r\n");
-        //        bool first = true;
-        //        foreach (string portName in module.PortParameterNameList)
-        //        {
-        //            if (!first) sb.Append(",\r\n");
-        //            sb.Append("\t");
-        //            sb.Append(".");
-        //            sb.Append(portName);
-        //            sb.Append("\t( {");
-        //            sb.Append(i.ToString());
-        //            i++;
-
-        //            if (
-        //                module.Parameters.ContainsKey(portName) &&
-        //                module.Parameters[portName].Expression != null
-        //                )
-        //            {
-        //                initials.Add(module.Parameters[portName].Expression.CreateString());
-        //            }
-        //            else
-        //            {
-        //                initials.Add("");
-        //            }
-
-        //            sb.Append("} )");
-        //            first = false;
-        //        }
-        //        sb.Append("\r\n) ");
-        //    }
-
-        //    sb.Append("{");
-        //    sb.Append(i.ToString());
-        //    i++;
-        //    initials.Add(instanceName);
-        //    sb.Append("}");
-
-        //    sb.Append(" (\r\n");
-        //    int j = 0;
-        //    foreach (Verilog.Variables.Port port in module.Ports.Values)
-        //    {
-        //        sb.Append("\t.");
-        //        sb.Append(port.Name);
-        //        sb.Append("\t( {");
-        //        sb.Append(i.ToString());
-        //        i++;
-        //        initials.Add(" ");
-        //        sb.Append("} )");
-        //        if (j != module.Ports.Count - 1) sb.Append(",");
-        //        sb.Append("\r\n");
-        //        j++;
-        //    }
-        //    sb.Append(");");
-
-        //    return sb.ToString();
-        //}
-
-        //private List<string> initials = new List<string>();
-
-        //public override void Aborted()
-        //{
-        //    codeEditor.Controller.CodeEditor.ClearHighlight();
-        //    document = null;
-        //    base.Aborted();
-        //}
-
-        //private List<int> startIndexs = new List<int>();
-        //private List<int> lastIndexs = new List<int>();
-
-        //public override void BeforeKeyDown(object sender, KeyEventArgs e, codeEditor.CodeEditor.AutoCompleteForm autoCompleteForm)
-        //{
-        //    if (autoCompleteForm == null || autoCompleteForm.Visible == false)
-        //    {
-        //        if (e.KeyCode == Keys.Return || e.KeyCode == Keys.Escape)
-        //        {
-        //            bool moved;
-        //            moveToNextHighlight(out moved);
-        //            if (!moved) codeEditor.Controller.CodeEditor.AbortInteractiveSnippet();
-        //            e.Handled = true;
-        //        }
-        //    }
-        //}
-        //public override void AfterKeyDown(object sender, KeyEventArgs e, codeEditor.CodeEditor.AutoCompleteForm autoCompleteForm)
-        //{
-
-        //}
-
-        //bool skipFirstHandle = true;
-        //public override void AfterAutoCompleteHandled(object sender, KeyEventArgs e, codeEditor.CodeEditor.AutoCompleteForm autoCompleteForm)
-        //{
-        //    if (skipFirstHandle)
-        //    {
-        //        skipFirstHandle = false;
-        //        return;
-        //    }
-        //    if (e.Handled) // closed
-        //    {
-        //        int i = codeEditor.Controller.CodeEditor.GetHighlightIndex(document.CaretIndex);
-        //        i++;
-        //        if(i+1 >= initials.Count)
-        //        {
-        //            codeEditor.Controller.CodeEditor.AbortInteractiveSnippet();
-        //            return;
-        //        }
-
-        //        codeEditor.Controller.CodeEditor.SelectHighlight(i);
-        //        codeEditor.Controller.CodeEditor.AbortInteractiveSnippet();
-        //        codeEditor.Controller.CodeEditor.RequestReparse();
-        //    }
-        //}
-
-
-        //private void moveToNextHighlight(out bool moved)
-        //{
-        //    moved = false;
-        //    moved = false;
-        //    int i = codeEditor.Controller.CodeEditor.GetHighlightIndex(document.CaretIndex);
-        //    if (i == -1) return;
-        //    i++;
-        //    if (i >= initials.Count) return;
-
-        //    codeEditor.Controller.CodeEditor.SelectHighlight(i);
-        //    moved = true;
-        //}
     }
 }
